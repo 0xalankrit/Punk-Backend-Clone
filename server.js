254b1f945187eca2784deb30a01fb43154072c37
0xalankrit <alankritnokhwal@gmail.com>
2022-10-07 15:04:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+let cors = require("cors");
+
+const { updatePrice } = require("./controller/updatePricing");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ ok: true, msg: "Crypt3DPunks Backend" });
+});
+
+app.get("/test", (req, res) => {
+  res.send("<h3><i>It's working 🤗</i></h3>")
+})
+
+app.get("/start", updatePrice);
+
+const PORT = process.env.PORT || 3000;
+//Start the server
+app.listen(PORT, () => {
+  console.log(`The application started successfully on port ${PORT}`);
+});
